e48bb7c7adcf16e456091010f27aab03c1e9be42
Chamara Sasmith <chamarasasmith.profiles@gmail.com>
2020-06-04 11:18:35 +0530

    Add project files.

---
+++ Files_Zip_Demo1/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Files_Zip_Demo1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string startPath = @"File_List";
+                string zipPath = @"Zip_File\result.zip";
+
+                ZipFile.CreateFromDirectory(startPath, zipPath);
+
+                MessageBox.Show("Success");
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string zipPath = @"Zip_File\result.zip";
+                string extractPath = @"Extract";
+
+                ZipFile.ExtractToDirectory(zipPath, extractPath);
+
+                MessageBox.Show("Success");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
